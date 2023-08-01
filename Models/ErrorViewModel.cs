f3579f3889257f46c9811b36a42a27155e45a2b2
MATÍAS ABOUDARA <47701750@ort>
2023-08-01 09:01:44 -0300

    Creación del proyecto

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace TP7_Aboudara_Garbarino_Kalinscky.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}

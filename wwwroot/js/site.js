e14e5516419408e9e387ea487bce15fa1fd41197
MATÍAS ABOUDARA <47701750@ort>
2023-09-05 09:11:11 -0300

    o

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -24,34 +24,43 @@ function elegirCategoria(id) {
     }
    
 }
+let tiempo = true;
 function respuesta(id){
     document.getElementById('Respuesta').value = id;
     document.getElementById('V').style = "background-color: rgb(32, 255, 32); color: white;";
+    document.getElementById('V').disabled = true;
     document.getElementById('F').style = "background-color: red; color: white;";
+    document.getElementById('F').disabled = true;
     document.getElementById('F').id = "F1";
     document.getElementById('F').style = "background-color: red; color: white;";
+    document.getElementById('F').disabled = true;
     document.getElementById('F').id = "F2";
     document.getElementById('F').style = "background-color: red; color: white;";
+    document.getElementById('F').disabled = true;
+    document.getElementById('tiempo').hidden = true;
     document.getElementById('Continuar').hidden = false;
+    tiempo = false;
 }
 const bar = document.querySelector('.bar');
-    const countdownElement = document.getElementById('countdown');
-    const countdownDuration = 30; // Duración del contador en segundos
-    let remainingTime = countdownDuration;
+const countdownElement = document.getElementById('countdown');
+const countdownDuration = 21; // Duración del contador en segundos
+let remainingTime = countdownDuration;
 
-    function updateBar() {
-      const progress = (countdownDuration - remainingTime) / countdownDuration;
-      const barWidth = `${progress * 100}%`;
-      bar.style.width = barWidth;
-    }
+function updateBar() {
+  const progress = (countdownDuration - remainingTime + 1) / countdownDuration;
+  const barWidth = `${progress * 100}%`;
+  bar.style.width = barWidth;
+}
 
-    function updateCounter() {
-      if (remainingTime > 0) {
-        remainingTime--;
-        updateBar();
-        countdownElement.textContent = remainingTime;
-        setTimeout(updateCounter, 1000); // Actualizar cada segundo
-      }
-    }
-
-    updateCounter();
+function updateCounter() {
+  if (remainingTime > 0 && tiempo) {
+    remainingTime--;
+    updateBar();
+    countdownElement.textContent = remainingTime;
+    setTimeout(updateCounter, 1000); // Actualizar cada segundo
+  }
+  if (remainingTime == 0){
+    respuesta(document.getElementById('F').className);
+  }
+}
+updateCounter();
